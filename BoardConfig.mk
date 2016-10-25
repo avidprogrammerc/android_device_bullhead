eacfd576757cdfc0a4818d2753f8c24adfeb0ce2
Chris Conley <chrisc93@vt.edu>
2016-10-24 21:29:48 -0400

    BoardConfig: use GCC 6.x instead of 4.9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ ENABLE_CPUSETS := true
 TARGET_NO_BOOTLOADER := true
 
 # Inline kernel building
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-6.x-kernel/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
 TARGET_KERNEL_CONFIG := phasma_defconfig
